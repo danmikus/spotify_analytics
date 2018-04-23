4b5684a330be64645347729cff1c595cc20c5409
Dan Mikus <danmikus@danmikus-macbookpro.roam.corp.google.com>
2018-04-22 22:40:53 -0400

    Finished working on streaming data to BQ

--- app/bqspot/bqlib.py
+++ app/bqspot/bqlib.py
@@ -1,21 +1,39 @@
 from google.cloud import bigquery
 
-def create_bq_client():
-    client = bigquery.Client.from_service_account_json('credentials/bq_key.json')
+def create_bq_client(path):
+    client = bigquery.Client.from_service_account_json(path)
 
     return client
 
-def dataset_creation(client, dataset_name):
+def create_taxonomy(client, dataset_name, table_struct):
+
     dataset_list = [name.dataset_id for name in list(client.list_datasets())]
 
+    # Check if dataset name exists and if not, create it
     if dataset_name not in dataset_list:
         dataset_ref = client.dataset(dataset_name)
         dataset = bigquery.Dataset(dataset_ref)
-        dataset.description = 'Dataset for Spotify Analysis'
+        dataset.description = 'Dataset for Analysis'
         dataset = client.create_dataset(dataset)
+    else:
+        dataset_ref = client.dataset(dataset_name)
 
-if __name__ == "__main__":
-    bq_client = create_bq_client()
-    dataset_name = "spotify_dataset"
+    tables = [table.table_id for table in list(client.list_tables(dataset_ref))]
 
-    dataset_creation(bq_client, dataset_name)
+    for item in table_struct:
+        if item['name'] not in tables:
+            table_ref = dataset_ref.table(item['name'])
+            table = bigquery.Table(table_ref, schema=item['schema'])
+            table.partitioning_type = 'DAY'
+            table.partition_expiration = 2592000000
+            client.create_table(table)
+
+def load_data(client, data, dataset):
+
+    dataset_ref = client.dataset(dataset)
+
+    for table_data in data:
+
+        table_ref = dataset_ref.table(table_data['name'])
+        table = client.get_table(table_ref)
+        client.insert_rows(table, table_data['data'])
--- app/spotify_data.py
+++ app/spotify_data.py
@@ -1,4 +1,5 @@
-from bqspot import spotlib
+from bqspot import spotlib, bqlib
+from google.cloud import bigquery
 
 def spotify_data(path, user, playlist):
 
@@ -7,16 +8,153 @@ def spotify_data(path, user, playlist):
     top_artists = [track.artist_id for track in top_tracks]
     artist_information = spotlib.get_artists(spotify, top_artists) # Populates Artist information table from top tracks
 
-    spot_data = {'tracks' : top_tracks, 'artists' : artist_information}
+    tracks_table_data = []
+    features_table_data = []
+    artist_table_data = []
+    genre_table_data = []
+    top_artist_tracks_table_data = []
+    related_artists_table_data = []
+
+
+    for track in top_tracks:
+        temp_tracks = (track.artist,
+                       track.artist_id,
+                       track.name,
+                       track.name_id,
+                       track.popularity,
+                       track.explicit,
+                       track.preview,
+                       track.duration_sec())
+        tracks_table_data.append(temp_tracks)
+
+    for features in top_tracks:
+        temp_features = (features.features['id'],
+                         features.features['danceability'],
+                         features.features['energy'],
+                         features.features['key'],
+                         features.features['loudness'],
+                         features.features['mode'],
+                         features.features['speechiness'],
+                         features.features['acousticness'],
+                         features.features['instrumentalness'],
+                         features.features['liveness'],
+                         features.features['valence'],
+                         features.features['tempo'])
+
+        features_table_data.append(temp_features)
+
+    for artist in artist_information:
+        temp_artists = (artist.artist,
+                        artist.artist_id,
+                        artist.followers,
+                        artist.popularity
+                        )
+
+        artist_table_data.append(temp_artists)
+
+    for artist in artist_information:
+        for genre in artist.genres:
+            temp_genre = (artist.artist_id,
+                          genre
+                          )
+            genre_table_data.append(temp_genre)
+
+    for artist in artist_information:
+        for top_track in artist.top_tracks:
+            temp_top_tracks = (artist.artist_id,
+                               top_track
+                               )
+            top_artist_tracks_table_data.append(temp_top_tracks)
+
+    for artist in artist_information:
+        for related_artist in artist.related_artists:
+            temp_related_artists = (artist.artist_id,
+                               related_artist
+                               )
+            related_artists_table_data.append(temp_related_artists)
+
+    spot_data = [{"name" : "tracks", "data" : tracks_table_data},
+                 {"name" : "features", "data" : features_table_data},
+                 {"name" : "artists", "data" : artist_table_data},
+                 {"name" : "genres", "data" : genre_table_data},
+                 {"name" : "top_artist_tracks", "data" : top_artist_tracks_table_data},
+                 {"name" : "related_artists", "data" : related_artists_table_data}
+                 ]
 
     return spot_data
+
+def bq_creation(creds_path, data):
+    bqclient = bqlib.create_bq_client(creds_path)
+
+    dataset_name = "spotify_dataset"
+
+    tracks_schema = [
+        bigquery.SchemaField('artist', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('artist_id', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('name', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('name_id', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('popularity', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('duration_sec', 'INTEGER', mode='NULLABLE'),
+        bigquery.SchemaField('explicit', 'BOOLEAN', mode='NULLABLE'),
+        bigquery.SchemaField('preview', 'STRING', mode='NULLABLE')
+    ]
+
+    features_schema = [
+        bigquery.SchemaField('name_id', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('danceability', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('energy', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('key', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('loudness', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('mode', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('speechiness', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('acousticness', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('instrumentalness', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('liveness', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('valence', 'FLOAT', mode='NULLABLE'),
+        bigquery.SchemaField('tempo', 'FLOAT', mode='NULLABLE')
+    ]
+
+    artists = [
+        bigquery.SchemaField('artist', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('artist_id', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('followers', 'INTEGER', mode='NULLABLE'),
+        bigquery.SchemaField('popularity', 'FLOAT', mode='NULLABLE'),
+    ]
+
+    genres = [
+        bigquery.SchemaField('artist_id', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('genre', 'STRING', mode='NULLABLE')
+    ]
+
+    top_artist_tracks_schema = [
+        bigquery.SchemaField('artist_id', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('name', 'STRING', mode='NULLABLE')
+    ]
+
+    related_artists_schema = [
+        bigquery.SchemaField('artist_id', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('related_name', 'STRING', mode='NULLABLE')
+    ]
+
+    table_struct = [{'name' : 'tracks', 'schema' : tracks_schema},
+                    {'name' : 'features', 'schema' : features_schema},
+                    {'name' : 'artists', 'schema' : artists},
+                    {'name' : 'genres', 'schema' : genres},
+                    {'name' : 'top_artist_tracks', 'schema' : top_artist_tracks_schema},
+                    {'name' : 'related_artists', 'schema' : related_artists_schema},
+                    ]
+
+    bqlib.create_taxonomy(bqclient, dataset_name, table_struct)
+    bqlib.load_data(bqclient, data, dataset_name)
+
+
 
 if __name__ == "__main__":
 
     user = 'spotify'
     user_playlist = '37i9dQZF1DXcBWIGoYBM5M'
-    path = 'bqspot/credentials/spot.json'
+    spot_path = 'bqspot/credentials/spot.json'
+    bq_path = 'bqspot/credentials/bq_key.json'
 
-    data = spotify_data(path, user, user_playlist)
-
-    import pdb; pdb.set_trace()
+    data = spotify_data(spot_path, user, user_playlist)
+    bq_creation(bq_path, data)
